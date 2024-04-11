00690832497ebd2a5b490307855fac3cb4ccdf3f
Andó Attila <andoattila698@gmail.com>
2024-04-11 15:44:22 +0200

    Api

---
+++ Data/JWTTokenAPIContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JWTTokenAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace JWTTokenAPI.Data
+{
+    public class JWTTokenAPIContext : IdentityDbContext<ApplicationUser>
+    {
+        public JWTTokenAPIContext (DbContextOptions<JWTTokenAPIContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<JWTTokenAPI.Models.Company> Company { get; set; } = default!;
+    }
+}
